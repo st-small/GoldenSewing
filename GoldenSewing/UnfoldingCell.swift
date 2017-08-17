57b1144de8478da0c3ee588b63f336634b973277
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-08-17 17:10:29 +0300

    Finished foldingCells feature

---
+++ GoldenSewing/UnfoldingCell.swift
@@ -0,0 +1,21 @@
+//
+//  UnfoldingCell.swift
+//  GoldenSewing
+//
+//  Created by Stanly Shiyanovskiy on 16.08.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class UnfoldingCell: FoldingCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
